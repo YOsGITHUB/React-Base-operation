0d352debaaf9945562f6408434a71b21f7d241a6
YOsGITHUB <87181417+YOsGITHUB@users.noreply.github.com>
2022-01-02 15:47:32 +0900

    React-base-operation-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
@@ -12,6 +13,18 @@ const App = () => {
   const onClickSwitchShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
   };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+    //numを監視している。処理の関心を分離できたり、最初の一回だけ実行する場合。
+  }, [num]);
 
   return (
     <>
